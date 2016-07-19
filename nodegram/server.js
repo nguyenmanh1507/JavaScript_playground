82fa499e5a8b8710961eaf17798a89b44bbaee4c
manh <manh@mmj.vn>
2016-07-19 10:59:46 +0700

    complete

--- nodegram/server.js
+++ nodegram/server.js
@@ -1,13 +1,31 @@
 const express = require('express')
 const app = express()
+const ig = require('instagram-node').instagram()
 
-app.use(express.static(__dirname))
+/*
+ * CONFIGURE THE APP
+ */
+// Tell node where to look for site resources
+app.use(express.static(__dirname + '/public'))
 
 // set template engine
-app.use('view engine', 'pug')
+app.set('view engine', 'pug')
 
+// configure instagram app with client_id, client_secret, and access_token
+ig.use({
+  access_token: '1717996661.1677ed0.feb24548844b4da48f2d3c9ccf1f67c2'
+})
+
+/*
+ * SET THE ROUTES
+ */
+// homepage route - our profile's images
 app.get('/', (req, res) => {
-  res.sendFile('index.html')
+  // use the instagram package to get our profile's media
+  ig.user_self_media_recent((err, medias, pagination, remaining, linit) => {
+    // render the home page and pass in the our profile's images
+    res.render('pages/index', {grams: medias})
+  })
 })
 
 app.listen(8080)
