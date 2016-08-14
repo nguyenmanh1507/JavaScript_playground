9b3ebc588600ee5ed7a149529627ad85e2155006
ngmanh <nguyenmanh1507@gmail.com>
2016-08-14 19:41:01 +0700

    loading images

--- greenSock_projects/dist/preloader.js
+++ greenSock_projects/dist/preloader.js
@@ -2,6 +2,24 @@
 
 var $circles = $('.circle');
 var tl = new TimelineMax();
+var imgUrls = ['https://s3-us-west-2.amazonaws.com/s.cdpn.io/409269/valley.jpg'];
+var imgUrlsLength = imgUrls.length;
+var imgs = [];
+var counter = 0;
+
+for (var i = 0; i < imgUrlsLength; i++) {
+  imgs.push($('<img>'));
+  imgs[i].load(loaderOut);
+  imgs[i].attr('src', imgUrls[i]);
+}
+
+function loaderOut() {
+  counter++;
+  console.log('Image is done loading', counter);
+  if (counter >= imgUrlsLength) {
+    tl.pause();
+  }
+}
 
 TweenMax.set($circles, { scale: 0 });
 tl.insert(TweenMax.staggerTo($circles.toArray(), 1, {
@@ -12,6 +30,7 @@ tl.insert(TweenMax.staggerTo($circles.toArray(), 1, {
 
 tl.insert(TweenMax.staggerTo($circles.toArray(), 1, {
   scale: 1.2,
+  boxShadow: '0 25px 25px rgba(0, 0, 0, 0.4)',
   repeat: -1,
   yoyo: true,
   ease: Power1.easeOut
--- greenSock_projects/src/preloader.js
+++ greenSock_projects/src/preloader.js
@@ -1,5 +1,24 @@
 const $circles = $('.circle')
 const tl = new TimelineMax()
+const imgUrls = ['https://s3-us-west-2.amazonaws.com/s.cdpn.io/409269/valley.jpg']
+const imgUrlsLength = imgUrls.length
+let imgs = []
+let counter = 0
+
+
+for (let i = 0; i < imgUrlsLength; i++) {
+  imgs.push($('<img>'))
+  imgs[i].load(loaderOut)
+  imgs[i].attr('src', imgUrls[i])
+}
+
+function loaderOut() {
+  counter++
+  console.log('Image is done loading', counter)
+  if (counter >= imgUrlsLength) {
+    tl.pause()
+  }
+}
 
 TweenMax.set($circles, {scale: 0})
 tl.insert(
@@ -13,6 +32,7 @@ tl.insert(
 tl.insert(
   TweenMax.staggerTo($circles.toArray(), 1, {
     scale: 1.2,
+    boxShadow: '0 25px 25px rgba(0, 0, 0, 0.4)',
     repeat: -1,
     yoyo: true,
     ease: Power1.easeOut,
